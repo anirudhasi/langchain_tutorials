a08c291e0fd52d28618ed860ac8c1dd1e80c953b
anirudhasi <55198467+anirudhasi@users.noreply.github.com>
2025-08-25 21:13:11 +0530

    Models Added

---
+++ test.py
@@ -0,0 +1,9 @@
+import langchain
+
+print(langchain.__version__)
+print("Hello, LangChain!")
+print("This is a test file.")
+print("This file is used to test the environment setup.")
+print("Everything is working fine!")
+print("Goodbye, LangChain!")
+print("End of the test file.")
